a86dfec962fa03cd7f0580a3a3e905e37a7fbd51
Sun1203 <84012715+Sun1203@users.noreply.github.com>
2022-11-13 21:32:08 +0900

    inital commit

---
+++ src/main/java/com/example/actiondoker/ActiondokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.actiondoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiondokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiondokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/actiondoker/ActiondokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actiondoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiondokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
