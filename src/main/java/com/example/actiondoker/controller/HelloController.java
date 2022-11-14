7033f8c61c666d9d1025958585e795402826c31f
Sun1203 <84012715+Sun1203@users.noreply.github.com>
2022-11-14 16:23:36 +0900

    Feat : secrets application.properties

--- src/main/java/com/example/actiondoker/controller/HelloController.java
+++ src/main/java/com/example/actiondoker/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.actiondoker.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Component
 public class HelloController {
+    @Value("${test.name}")
+    private String name;
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+
+        return "Hello World" + name;
     }
 }
